6fc8859efe2ad7d4d7f1e31ed0862c77ecb150dc
Roger <thrasher_royerr666@hotmail.com>
2020-05-13 19:16:45 -0500

    Updates

---
+++ precache-manifest.e2831395a443bdb1b4c47b1c7f16c22c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33f951b562b7f38c52694183013e281",
+    "url": "/Expense-Tracker-React/index.html"
+  },
+  {
+    "revision": "5a3d90efcb9c80c6739f",
+    "url": "/Expense-Tracker-React/static/css/main.bcfc0eee.chunk.css"
+  },
+  {
+    "revision": "f35695db11317516d110",
+    "url": "/Expense-Tracker-React/static/js/2.c3734ca7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker-React/static/js/2.c3734ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3d90efcb9c80c6739f",
+    "url": "/Expense-Tracker-React/static/js/main.67717ee6.chunk.js"
+  },
+  {
+    "revision": "cd8e78f2f58f8ed753f7",
+    "url": "/Expense-Tracker-React/static/js/runtime-main.20bff519.js"
+  }
+]);
